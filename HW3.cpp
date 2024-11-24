a5421810f31e35226253f865e3f7cf71bb92bd07
wendy3220 <wendy322061@gamil.com>
2024-11-24 18:37:55 +0800

    update

--- HW3.cpp
+++ HW3.cpp
@@ -1,19 +1,6 @@
 #include <iostream>     
 using namespace std; 
 
-class counter_ptr
-{
-private:
-	char * _name;
-	int_array_cell * _cell_ptr;
-public:
-	counter_ptr::counter_ptr(char * _name, int * memory)
-	{
-		this->_name = _name;
-		this->_cell_ptr = new int_array_cell(memory);
-	}
-};
-
 //template <class T>
 class int_array_cell
 {
@@ -21,19 +8,83 @@ private:
 	int _counter;
 	int * _memory;     
 public:
-	int_array_cell::int_array_cell(int * _memory)
+	int_array_cell(int * _memory)
 	{
 		this->_memory = _memory;
-		cout << "stdout 輸出: int_array_cell is allocated" << endl;
-		this->_counter++;
-		cout << "counter_ptr b is assigned to an int_array_cell: counter 1" << endl;
+		this->_counter = 1;
 	}
+	
+//	~int_array_cell()
+//	{
+//		delete[] this->_memory;
+//		this->_memory = nullptr;
+//	}
+//	
+	decrease()
+	{
+		this->_counter--;
+		if(this->_counter == 0)
+		{
+			delete[] this->_memory;
+			this->_memory = nullptr;
+			cout << "int_array_cell counter 0: deleted" << endl;
+		}
+	}
+	
+	increase()
+	{
+		this->_counter++;
+	}
+	
+	int getCounter()
+	{
+		return this->_counter; 
+	} 
 	//T  _memory;    //template  
 };
 
-
+class counter_ptr
+{
+private:
+	char * _name;
+	int_array_cell * _cell_ptr;
+public:
+	counter_ptr(char * _name, int * memory)
+	{
+		this->_name = _name;
+		this->_cell_ptr = new int_array_cell(memory);
+		cout << "int_array_cell is allocated" << endl;
+		cout << "counter_ptr " << this->_name << " is assigned to an int_array_cell: counter " << this->_cell_ptr->getCounter() << endl;
+	}
+	~counter_ptr()
+	{
+		delete[] this->_name;
+		
+	}
+	counter_ptr(char * _name)
+	{
+		this->_name = _name;
+		this->_cell_ptr = nullptr; 
+		cout << "counter_ptr " << this->_name << " is not assigned to an int_array_cell" << endl;
+	}
+	
+	counter_ptr& operator=(int * new_Memory)
+	{
+		this->_cell_ptr->decrease();
+		this->_cell_ptr = new int_array_cell(new_Memory);
+		cout << "int_array_cell is allocated" << endl;
+		cout << "counter_ptr " << this->_name << " is assigned to an int_array_cell: counter " << this->_cell_ptr->getCounter() << endl;		
+	}
+	counter_ptr& operator=(const counter_ptr& other)
+	{
+		this->_cell_ptr = other._cell_ptr;
+		this->_cell_ptr->increase();
+		cout << "int_array_cell counter is increased: counter " << this->_cell_ptr->getCounter() << endl;
+		cout << "counter_ptr a is assigned to an int_array_cell: counter " << this->_cell_ptr->getCounter() << endl;
+	}
+};
  
-void main()
+int main()
 {
 
 // PART 一  (35分)
@@ -42,21 +93,21 @@ void main()
 	// stdout 輸出: int_array_cell is allocated
 	// stdout 輸出: counter_ptr b is assigned to an int_array_cell: counter 1
 
-			// {
+			{
 
-			// 	b = new int[100]; 	//需要撰寫 counter_ptr operator=
+		 	b = new int[100]; 	//需要撰寫 counter_ptr operator=
 			// 	// stdout 輸出: int_array_cell counter 0: deleted
 			// 	// stdout 輸出: int_array_cell is allocated
 			// 	// stdout 輸出: counter_ptr b is assigned to an int_array_cell: counter 1
 
-			// 	counter_ptr a("a");	//需要撰寫 counter_ptr constructor(建構子) 接受一個參數
+			counter_ptr a("a");	//需要撰寫 counter_ptr constructor(建構子) 接受一個參數
 			// 	// stdout 輸出: counter_ptr a is not assigned to an int_array_cell
 
-			// 	a = b;			//需要撰寫 counter_ptr operator= 
+			a = b;			//需要撰寫 counter_ptr operator= 
 			// 	// stdout 輸出: int_array_cell counter is increased: counter 2
 			// 	// stdout 輸出: counter_ptr a is assigned to an int_array_cell: counter 2
 
-			// }
+			}
 			// // a leaves it scope:	//需要撰寫 counter_ptr destructor
 			// // stdout 輸出: counter_ptr a is deleted
 			// // stdout 輸出: int_array_cell counter is decreased: counter 1
